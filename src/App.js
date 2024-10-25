b8190ce62c3a09bdbd721be2a1317e0f68a36513
GaneshGupta3 <ganesh310104@gmail.com>
2024-10-25 12:13:05 +0530

    pushed

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import './App.css'
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [showResults, setShowResults] = useState(false);
+
+  // Fetch quiz questions from the backend
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      const response = await fetch('http://localhost:5000/questions');
+      const data = await response.json();
+      setQuestions(data);
+    };
+
+    fetchQuestions();
+  }, []);
+
+  const handleAnswer = (answer) => {
+    setUserAnswers([...userAnswers, answer]);
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      setShowResults(true);
+    }
+  };
+
+  const calculateScore = () => {
+    return userAnswers.reduce((score, answer, index) => {
+      return answer === questions[index].correct ? score + 1 : score;
+    }, 0);
+  };
+
+  return (
+    <div class="centerPlayGround">
+      <h1>Quick Quiz Challenge</h1>
+      {showResults ? (
+        <div>
+          <h2>Your Score: {calculateScore()} / {questions.length}</h2>
+        </div>
+      ) : (
+        questions.length > 0 && (
+          <div class="fullQuestion">
+            <h2 class="questionText">{questions[currentQuestionIndex].question}</h2>
+            <div class="questionsDiv">
+              {questions[currentQuestionIndex].options.map((option, index) => (
+                <button  class="optionButton" key={index} onClick={() => handleAnswer(option)}>
+                  {option}
+                </button>
+              ))}
+            </div>
+          </div>
+        )
+      )}
+    </div>
+  );
+};
+
+export default App;
